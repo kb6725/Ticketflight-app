db047cef33e33e7e4c16e9375ff32f1a365b4f5f
Evan Amara <Evan.Amara@my.ccsu.edu>
2020-05-04 10:16:19 -0400

    Remove old DB assets.

--- flight1/DB/dbo.table1.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Table]
-(
-	[code] VARCHAR(50) NOT NULL, 
-    [name] VARCHAR(50) NOT NULL, 
-    [pp] VARCHAR(50) NOT NULL, 
-    CONSTRAINT [PK_Table] PRIMARY KEY ([code]) 
-)
